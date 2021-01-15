4a26ef7794aa4aa573af89abd61ed10adf42b577
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-01-15 12:30:32 -0300

    Configure user controller and routes

--- src/router/router.go
+++ src/router/router.go
@@ -1,8 +1,13 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/routes"
+
+	"github.com/gorilla/mux"
+)
 
 // Generates a new router with configured routes
 func Generate() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return routes.Configure(r)
 }
