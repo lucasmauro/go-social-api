1454f774d72b9900788e98e4bc1d1237e5f764b1
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-01-15 14:41:07 -0300

    Include database config

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Load()
+
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
