958fafb5f7175ca13e3ed0a3399d2398acead3eb
Lucas Mauro <lucasmaurodesouza@gmail.com>
2021-01-23 01:32:52 -0300

    Sample posts data

--- sql/data.sql
+++ sql/data.sql
@@ -10,3 +10,9 @@ INSERT INTO followers (user_id, follower_id)
     	(1, 2),
         (1, 3),
         (2, 3);
+
+INSERT INTO posts (title, content, author_id)
+    VALUES
+        ("This is a post from user 1", "Such a nice post this is.", 1),
+        ("This is a post from user 2", "Such a nice post this is.", 2),
+        ("This is a post from user 3", "Such a nice post this is.", 3);
